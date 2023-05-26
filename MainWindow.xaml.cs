88b1e18529a3ba574712fa53cea27760483408bd
xiuyangzhe <769865744@qq.com>
2023-05-26 10:36:01 +0800

    添加分页

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,29 @@ namespace ximalayaMP3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _currentPage = 1;
+        private long _totalPageCount = 0;
+
+        public int CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                page.Text = value.ToString();
+            }
+        }
+
+        public long TotalPageCount
+        {
+            get => _totalPageCount;
+            set 
+            {
+                pageCount.Text = value.ToString();
+                _totalPageCount = value;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,6 +59,8 @@ namespace ximalayaMP3
         private void audioGet_Click(object sender, RoutedEventArgs e)
         {
             listView.ItemsSource = null;
+            CurrentPage = 1;
+            
             getTrackInfo();
         }
 
@@ -60,16 +85,26 @@ namespace ximalayaMP3
         {
             try
             {
+                var pageSize = 30;
                 listView.ItemsSource = null;
                 var client = new RestClient();
 
-                var request = new RestRequest($"https://www.ximalaya.com/revision/album/v1/getTracksList?albumId={albumId.Text}&pageNum=1&pageSize=30");
+                var request = new RestRequest($"https://www.ximalaya.com/revision/album/v1/getTracksList?albumId={albumId.Text}&pageNum={CurrentPage}&pageSize={pageSize}");
 
                 // The cancellation token comes from the caller. You can still make a call without it.
                 var result = await client.GetAsync<ResultData<AlbumData>>(request);
 
                 var list = new ObservableCollection<TrackInfoDetail>();
 
+                
+                total.Text = result.Data.TrackTotalCount.ToString();
+
+                var totalPage = result.Data.TrackTotalCount / pageSize;
+                if (result.Data.TrackTotalCount % pageSize > 0)
+                {
+                    totalPage += 1;
+                }
+                TotalPageCount = totalPage;
                 var no = 1;
                 result.Data.Tracks.ForEach(m =>
                 {
@@ -121,25 +156,102 @@ namespace ximalayaMP3
             getAlbum();
         }
 
+        private async void downloadAudio(string id, string title,string end) 
+        {
+            var src = await getAudio(id);
+            if (src != null)
+            {
+                var client = new RestClient();
+
+                var request = new RestRequest(src);
+
+                // The cancellation token comes from the caller. You can still make a call without it.
+                var result = await client.DownloadDataAsync(request);
+                if (result == null)
+                {
+                    //MessageBox.Show("下载失败");
+                    return;
+                }
+
+
+                var path = "";
+                Dispatcher.Invoke(() =>
+                {
+                    path = System.IO.Path.Combine(downloadAddress.Text, title + end);
+                });
+                File.WriteAllBytes(path, result);
+            }
+        }
+
         private async void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             var detail = listView.SelectedItem as TrackInfoDetail;
             if (detail != null) {
-                var src = await getAudio(detail.TrackId + "");
-                if (src != null) 
-                {
-                    var client = new RestClient();
-
-                    var request = new RestRequest(src);
-
-                    // The cancellation token comes from the caller. You can still make a call without it.
-                    var result = await client.DownloadDataAsync(request);
-                    var path = System.IO.Path.Combine(downloadAddress.Text, detail.Title + ".m4a");
-                    File.WriteAllBytes(path, result);
-                }
+                downloadAudio(detail.TrackId + "", detail.Title,".m4a");
             }
-            
-
         }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            var detail = listView.SelectedItem as TrackInfoDetail;
+            if (detail != null)
+            {
+                downloadAudio(detail.TrackId + "", detail.Title, ".mp3");
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            var list = new List<TrackInfoDetail>();
+            foreach (var m in listView.ItemsSource)
+            {
+                list.Add(m as TrackInfoDetail);
+            }
+            Parallel.ForEach(list, new ParallelOptions() 
+            {
+                MaxDegreeOfParallelism = 3
+            }, detail => 
+            {
+                downloadAudio(detail.TrackId + "", detail.Title, ".m4a");
+
+            });
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            var list = new List<TrackInfoDetail>();
+            foreach (var m in listView.ItemsSource)
+            {
+                list.Add(m as TrackInfoDetail);
+            }
+            Parallel.ForEach(list, new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 3
+            }, detail =>
+            {
+                downloadAudio(detail.TrackId + "", detail.Title, ".mp3");
+
+            });
+        }
+
+        private void lastPage_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPage > 1) 
+            {
+                CurrentPage -= 1;
+                getAlbum();
+            }
+        }
+
+        private void nextPage_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPage < TotalPageCount)
+            {
+                CurrentPage += 1;
+                getAlbum();
+            }
+        }
+
+
     }
 }
